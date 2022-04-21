149903855bfa2b8568749523139e54fccf98f06c
Dar <64095223+DarianRepo@users.noreply.github.com>
2022-04-20 20:06:08 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+/* 
+   TERMED.LOL GLOBAL VARIABLES
+   SET AN ID TO "@me" IF YOU DONT WANT IT TO GO ANYWHERE
+ */
+
+const maintag = "Discord#0000"; //main account tag
+const mainid = "12345678"; // main account id
+
+const 1alttag = "Alt#0000"; // alt 1 tag
+const 1altid = "987654"; // alt 1 id
+
+const 2alttag = "N/A"; // alt 2 tag
+const 2altid = "@me" // alt 2 id
